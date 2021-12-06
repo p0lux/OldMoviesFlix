f502f6c14457b92e2476db7df55998f050c53404
p0 <a>
2021-12-06 21:42:06 +0100

    Initial commit

---
+++ videos/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=220)
+    description = models.TextField(blank=True, null=True)
+    slug = models.SlugField(blank=True, null=True)
+    video_id = models.CharField(max_length=200)
+    # timestamp =
+    # updated =
+    # state =
+    # publish_timestamp =
---
+++ videos/tests.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from .models import Video
+
+
+class VideoModelTestCase(TestCase):
+    def setUp(self):
+        pass
